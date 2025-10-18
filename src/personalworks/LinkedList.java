a44f3409ecf84def3f85f0a4ac481570625994a2
OMORI <ozangacanozan@gmail.com>
2025-10-18 14:31:56 +0300

    Handling of edge cases and optimisation

--- src/personalworks/LinkedList.java
+++ src/personalworks/LinkedList.java
@@ -13,7 +13,7 @@ public class LinkedList {
         //Make a head if the list is empty
         if(head == null){
             head = newNode;
-            this.size = 1;
+            this.size++;
             return;
         //If a head already exists
         }else{
@@ -34,16 +34,31 @@ public class LinkedList {
         newNode.next = head;
         //New node is assigned as the new head
         head = newNode;
+        this.size++;
     }
     //Method to delete the node with given data
     public void delete(String gasName, int collision){
+        //Handle the case where the list is empty
+        if(head == null){
+            System.out.println("The list is empty!");
+            return;
+        }
+        //If head is the node to be deleted
+        //This is checked seperately because the deletion for middle and tail nodes check for compliance of next node
+        //This covers the case where the head is the only node
         if(head.gasName.equalsIgnoreCase(gasName) && head.collisions == collision){
             head = head.next;
+            this.size--;
+            return;
         }
         Node current = head;
-        while(!(current.next.gasName.equalsIgnoreCase(gasName) && current.next.collisions == collision)){
+        while(current.next != null) {
+            if(current.next.gasName.equalsIgnoreCase(gasName) && current.next.collisions == collision){
+                current.next = current.next.next;
+                size--;
+                return;
+            }
             current = current.next;
         }
-        current.next = current.next.next;
     }
 }
