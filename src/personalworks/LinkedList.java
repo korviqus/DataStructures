67586d7bdc34cd9301ad1afb330b2ebde7b8969b
OMORI <ozangacanozan@gmail.com>
2025-10-18 15:34:24 +0300

    Added printList method

--- src/personalworks/LinkedList.java
+++ src/personalworks/LinkedList.java
@@ -11,14 +11,14 @@ public class LinkedList {
     public void append(String gasName, int collisions){
         Node newNode = new Node(gasName, collisions);
         //Make a head if the list is empty
-        if(head == null){
-            head = newNode;
+        if(this.head == null){
+            this.head = newNode;
             this.size++;
             return;
         //If a head already exists
         }else{
             //Temporary node
-            Node current = head;
+            Node current = this.head;
             //Keeps iterating through nodes until we reach the tail
             while(current.next != null){
                 current = current.next;
@@ -31,33 +31,42 @@ public class LinkedList {
     public void prepend(String gasName, int collisions){
         Node newNode = new Node(gasName, collisions);
         //New node points to original head
-        newNode.next = head;
+        newNode.next = this.head;
         //New node is assigned as the new head
-        head = newNode;
+        this.head = newNode;
         this.size++;
     }
     //Method to delete the node with given data
     public void delete(String gasName, int collision){
         //Handle the case where the list is empty
-        if(head == null){
+        if(this.head == null){
             System.out.println("The list is empty!");
             return;
         }
         //If head is the node to be deleted
         //This is checked seperately because the deletion for middle and tail nodes check for compliance of next node
         //This covers the case where the head is the only node
-        if(head.gasName.equalsIgnoreCase(gasName) && head.collisions == collision){
-            head = head.next;
+        if(this.head.gasName.equalsIgnoreCase(gasName) && this.head.collisions == collision){
+            this.head = this.head.next;
             this.size--;
             return;
         }
-        Node current = head;
+        Node current = this.head;
         while(current.next != null) {
             if(current.next.gasName.equalsIgnoreCase(gasName) && current.next.collisions == collision){
                 current.next = current.next.next;
                 size--;
                 return;
             }
+            current = current.next;
+        }
+    }
+    //A method to print the elements of the list
+    public void printList(){
+        Node current = this.head;
+        while(current != null){
+            System.out.printf("Name: %s\nCollisions: %d\n", current.gasName, current.collisions);
+            System.out.print("------------\n");
             current = current.next;
         }
     }
